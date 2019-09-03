9fb589ada50e42455e5a96b0524e7fe17f567011
Thiago Dong Chen <thiagodchen@gmail.com>
2019-09-03 14:57:10 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,10 +7,18 @@ class UnitConverterSkill(MycroftSkill):
     def __init__(self):
         super(UnitConverterSkill, self).__init__(name="UnitConverterSkill")
 
+    def initialize(self):
+        self.register_intent_file('convert', self.handle_convert_intent)
+
     @intent_handler(IntentBuilder("LaunchIntent").require("launch"))
     def handle_launch_intent(self, message):
         print(message)
         self.speak("Hi welcome to unit converter skill")
+
+    def handle_convert_intent(self, message):
+        type = message.data.get("type")
+
+        self.speak("The type spoken is " + type)
 
 def create_skill():
     return UnitConverterSkill()
